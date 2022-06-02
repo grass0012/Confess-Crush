77336932d490d9687a86228ba1bb4b9a305aaeca
grass0012 <106756283+grass0012@users.noreply.github.com>
2022-06-02 22:51:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const CONFIG = {
     question: ' t đã ngốn 1 tiếng đồng hồ loay hoay cái này nè ',
     btnReply: ' thưn quá z',
     reply: 'vì siu quá nên mai t bao Quỳnh cốc nước nè :>>',
-    mess: 'ỏoooooooo',
+    mess: 'ỏooo',
     messDesc: 'kkk, z thuii, thui ngủ ikk mai còn học vănn :>',
     btnAccept: 'byeeee',
     messLink: 'https://www.youtube.com/watch?v=Zkgzg5wK-4A' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
