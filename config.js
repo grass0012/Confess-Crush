abe4d1d993b7cbcf6c7fe645945e0c797d80ce7f
grass0012 <106756283+grass0012@users.noreply.github.com>
2022-06-02 22:17:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "190205",
+    introTitle: 'Thái Sơn chăng',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
