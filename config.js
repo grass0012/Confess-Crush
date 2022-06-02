b13b9d5a0db7c2ea2b12510d72655709b77042ac
grass0012 <106756283+grass0012@users.noreply.github.com>
2022-06-02 22:11:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: " Những suy nghĩ của Quỳnh",
+    introTitle: 'Sao h tr',
+    introDesc: `Trầm kẽmm`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: 'Haizz, khổ thân lắm í tr',
+    desc: 'Nma nói j thì ns, t cute váiii ',
+    btnYes: 'đúmm roàiii',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    question: 'Sao càng lớn càng vớ vẩn z tr :>',
+    btnReply: 'ai bít tr :>',
+    reply: 'Chắc do m cute ở mọi vũ trụ ây mà',
+    mess: 'T biết mà :>>',
+    messDesc: 'Ai đó cíu t khỏi mí cái suy nghĩ này ik tr', 
+    btnAccept: 'Okiiiii nhó',
+    messLink: 'https://www.youtube.com/watch?v=s2ce-BRttgk' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
